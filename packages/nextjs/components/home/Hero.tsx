3e2cadc41cf3ef3d9307b92c5e65b1fceb7a3ea9
Angry Coding <mailidanielifechukwu@gmail.com>
2023-06-01 12:16:43 +0100

    hero section

---
+++ packages/nextjs/components/home/Hero.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+/**
+ * Home Page Hero Section
+ */
+export const Hero = () => {
+  return (
+    <div className="flex lg:flex-row flex-col  h-[95vh] justify-center p-5 md:p-8 lg:p-0">
+      <div className="flex flex-col lg:w-1/2  relative justify-center ">
+        <div className="absolute w-full lg:left-32 lg:top-48 lg:p-0">
+          <h1 className="font-bakbak-one text-5xl">
+            Experience Music on a Deeper Level. Connect, Support, and Amplify Your Favourite Artists
+          </h1>
+          <p className="font-abel"> Elevate your favorite artists while shaping the music scene of tomorrow.. </p>
+          <div className="mt-10">
+            <button className="btn btn-md  btn-primary font-changa capitalize  text-sm px-12">Get Started Now</button>
+          </div>
+        </div>
+      </div>
+      <div className=" relative flex flex-col justify-between lg:w-1/2 ">
+        <div className=" absolute h-full w-[60vw] right-0  bg-[url('/assets/background.png')] bg-[length:100%_100%] " />
+      </div>
+    </div>
+  );
+};
