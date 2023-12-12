9f43f18f64cfe9f9e2c97c44c8c6bfdfe3ef986b
Angry Coding <mailidanielifechukwu@gmail.com>
2023-12-12 12:41:27 +0100

    Spotify Types And Session Types

---
+++ packages/nextjs/types/session.ts
@@ -0,0 +1,14 @@
+import { DefaultSession } from "next-auth";
+
+interface MyUser {
+  name?: string | null;
+  email?: string | null;
+  picture?: string | null;
+  image?: string | null;
+  accessToken?: string | null;
+}
+
+export interface MySession extends Omit<DefaultSession, "user"> {
+  user?: MyUser;
+  expires: string;
+}
---
+++ packages/nextjs/types/spotify.ts
@@ -0,0 +1,73 @@
+interface Image {
+  height: number | null;
+  url: string | null;
+  width: number | null;
+}
+
+export interface Album {
+  id: string;
+  name: string;
+  artists: [Artist];
+  images?: [Image];
+  album_type?: string;
+  release_date?: string;
+  tracks?: {
+    total: number;
+    items: Track[];
+  };
+}
+
+export interface Artist {
+  id: string;
+  name: string;
+  images?: [Image];
+  followers?: {
+    total: number;
+  };
+  genres?: [string];
+}
+
+export interface Track {
+  id: string;
+  name: string;
+  album: Album;
+  artists: [Artist];
+  duration_ms: number;
+  preview_url: string;
+}
+
+export interface PlaylistType {
+  description?: string;
+  id: string;
+  followers?: {
+    total?: number;
+  };
+  images?: [Image];
+  name: string;
+  owner?: {
+    id: string;
+    display_name?: string;
+  };
+  items?: [{ added_at: string; track: Track }];
+  tracks?: {
+    items?: [{ added_at: string; track: Track }];
+    total: number;
+  };
+  type?: string;
+  total?: number;
+}
+
+export interface SearchResults {
+  albums?: {
+    items: Album[];
+  };
+  artists?: {
+    items: Artist[];
+  };
+  playlists?: {
+    items: PlaylistType[];
+  };
+  tracks?: {
+    items: Track[];
+  };
+}
