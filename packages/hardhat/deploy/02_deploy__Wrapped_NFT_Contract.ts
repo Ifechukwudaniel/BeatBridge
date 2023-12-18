fb5e43d259bf392546147e9e1ec02d86d157cdb2
Angry Coding <mailidanielifechukwu@gmail.com>
2023-12-18 22:17:16 +0100

    deploy Scripts

--- packages/hardhat/deploy/02_deploy__Wrapped_NFT_Contract.ts
+++ packages/hardhat/deploy/02_deploy__Wrapped_NFT_Contract.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { parseEther } from "ethers/lib/utils";
 
 const deployERC721FactoryContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
@@ -7,28 +8,28 @@ const deployERC721FactoryContract: DeployFunction = async function (hre: Hardhat
 
   await deploy("WrappedNFTBase", {
     from: deployer,
-    args: ["Spotify Wrapped 2023", "WRAP2023"],
+    args: [" Premium Spotify Wrapped 2023", "WRAP2023", 1000, parseEther("0")],
     log: true,
     autoMine: true,
   });
 
   await deploy("WrappedNFTBase", {
     from: deployer,
-    args: ["Spotify Wrapped Song", "SONGWRAP2023"],
+    args: ["Premium Spotify Wrapped Song", "SONGWRAP2023", 1000, parseEther("0")],
     log: true,
     autoMine: true,
   });
 
   await deploy("WrappedNFTBase", {
     from: deployer,
-    args: ["Spotify Wrapped Artist", "ARTWRAP2023"],
+    args: ["Premium Spotify Wrapped Artist", "ARTWRAP2023", 1000, parseEther("0")],
     log: true,
     autoMine: true,
   });
 
   await deploy("WrappedNFTBase", {
     from: deployer,
-    args: ["Spotify Wrapped Genre", "GENWRAP2023"],
+    args: ["Premium Spotify Wrapped Genre", "GENWRAP2023", 1000, parseEther("0")],
     log: true,
     autoMine: true,
   });
